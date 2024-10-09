4e3f3074ac057b299b09f9b37cbc7cfb92e1704d
saraicht <sarai.choqquepata@tecsup.edu.pe>
2024-10-09 18:18:44 -0500

    Initial Commit

---
+++ saraiSoundBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  saraiSoundBoard
+//
+//  Created by sarai choqquepata tapia on 9/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
